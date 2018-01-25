86678fce3817388641db3d0f4002b3f8d409377d
mprahl <matt_prahl@live.com>
2018-01-25 14:12:36 -0500

    Remove invalid imports for TestKerberosAuthentication and fix its styling

--- pdcupdater/tests/handler_tests/test_kerberos_auth.py
+++ pdcupdater/tests/handler_tests/test_kerberos_auth.py
@@ -1,23 +1,22 @@
-import pytest
-import requests_kerberos
-from mock import patch, Mock
-import pdcupdater.utils
-from test.test_support import EnvironmentVarGuard
 import os
 
-class TestKerberosAuthentication(object):
+from mock import patch, Mock
 
-	@patch('os.path.exists', return_value=True)
-	@patch('requests_kerberos.HTTPKerberosAuth')
-	@patch('requests.get')
-	def test_get_token(self, requests_get, kerb_auth, os_path):
-		self.url = 'https://pdc.fedoraproject.org/rest_api/v1/'
-		set_env=patch.dict(os.environ,{'KRB5_CLIENT_KTNAME': '/etc/foo.keytab'})
-		requests_rv = Mock()
-		requests_rv.json.return_value = {"token": "12345"}
-		requests_get.return_value = requests_rv
-		set_env.start()
-		rv = pdcupdater.utils.get_token(self.url,
-			'/etc/foo.keytab')
-		set_env.stop()
-		assert rv == '12345'
+import pdcupdater.utils
+
+
+class TestKerberosAuthentication(object):
+    @patch('os.path.exists', return_value=True)
+    @patch('requests_kerberos.HTTPKerberosAuth')
+    @patch('requests.get')
+    def test_get_token(self, requests_get, kerb_auth, os_path):
+        self.url = 'https://pdc.fedoraproject.org/rest_api/v1/'
+        set_env = patch.dict(
+            os.environ, {'KRB5_CLIENT_KTNAME': '/etc/foo.keytab'})
+        requests_rv = Mock()
+        requests_rv.json.return_value = {"token": "12345"}
+        requests_get.return_value = requests_rv
+        set_env.start()
+        rv = pdcupdater.utils.get_token(self.url, '/etc/foo.keytab')
+        set_env.stop()
+        assert rv == '12345'
